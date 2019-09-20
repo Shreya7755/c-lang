cfedd609d11b93f09453cd1a7ada8c7897afba9c
Shreya7755 <55089270+Shreya7755@users.noreply.github.com>
2019-09-20 15:46:00 +0530

    Create day3_task5.c

---
+++ day3_task5.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<math.h>
+int main(void)
+{
+    int a,b;
+    a=5;
+    b=7;
+    printf("the two integers are %d %d\n",a,b);
+     if(a==b)
+     {
+        printf("%d is equal to %d\n",a,b);
+     }
+     if(a!=b)
+     {
+        printf("%d is not equal to %d\n",a,b);
+     }
+     if(a<b)
+     {
+         printf("%d is less than %d\n",a,b);
+     }
+     if(a>b)
+     {
+         printf("%d is greater than %d\n",a,b);
+     }
+}
